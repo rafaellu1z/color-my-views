0e02f28dce6081d7b5fed26c86021f0ceb526ea5
Rafael Luiz <rafael.s.luiz@gmail.com>
2021-06-26 10:27:23 -0300

    Implementacao de compartilhamento de tela

--- app/src/main/java/com/devventure/colormyviews/MainActivity.kt
+++ app/src/main/java/com/devventure/colormyviews/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.devventure.colormyviews
 
+import android.Manifest
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.devventure.colormyviews.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -106,5 +122,74 @@ class MainActivity : AppCompatActivity() {
 
         editor.commit()
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun shareScreenScreenshot(view: View) {
+        var path = ""
+        if (checkAppPermissions()) {
+            val rootView = window.decorView.findViewById<View>(android.R.id.content)
+            val bitmap = takeScreenshot(rootView)
+            if (bitmap != null) {
+                path = storeScreenshot(bitmap)
+            }
+            shareScreenshot(path)
+        } else {
+            Toast.makeText(this, R.string.ask_permissions, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkAppPermissions(): Boolean {
+        val permission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ), 1
+            )
+        }
+        return permission == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun takeScreenshot(rootView: View): Bitmap? {
+        val bitmap = Bitmap.createBitmap(rootView.width, rootView.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        val bgDrawable = rootView.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        rootView.draw(canvas)
+        return bitmap
+    }
+
+    private fun storeScreenshot(bitmap: Bitmap): String {
+        return MediaStore.Images.Media.insertImage(contentResolver, bitmap, imgName(), null)
+    }
+
+    private fun imgName(): String {
+        return "Screenshot" + Date().time
+    }
+
+    private fun shareScreenshot(path: String) {
+        val imgToShare: Uri = Uri.parse(path)
+        val intent = Intent()
+        intent.action = Intent.ACTION_SEND
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_SUBJECT, "My art")
+        intent.putExtra(Intent.EXTRA_TEXT, "my beautiful drawing")
+        intent.putExtra(Intent.EXTRA_STREAM, imgToShare)
+
+        try {
+            startActivity(Intent.createChooser(intent, "Share Screenshot"))
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "No app Avaliable", Toast.LENGTH_SHORT).show()
+        }
     }
 }
